557d30bd1678af5297da20cc1acb2a00bded6c7d
Tiger Taylor <tiger@tigertaylor.xyz>
2025-08-03 17:10:21 +0100

    Set up CLI and use init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+//! Rebar - A new version control system written in Rust
+
+use clap::{Parser, Subcommand};
+
+/// A new version control system written in Rust
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Initialize a new .rebar repository
+    Init,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        // Create the main .rebar directory
+        Command::Init => commands::init(),
+    }
 }
